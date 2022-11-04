c07655ed4de00e46ed425dd8956f6a278f23bcf2
Jonas Wolf <jonas.wolf@jonaswolf.org>
2022-11-04 23:50:41 +0100

    Should work now.

--- build.rs
+++ build.rs
@@ -2,8 +2,10 @@ fn main() {
     // If we set CARGO_PKG_VERSION this way, then it will override the default value, which is
     // taken from the `version` in Cargo.toml.
     if let Ok(val) = std::env::var("GSN2X_VERSION") {
-        let version = env!("CARGO_PKG_VERSION");
-        println!("cargo:rustc-env=CARGO_PKG_VERSION={}-{}", version, val);
+        if !val.is_empty() {
+            let version = env!("CARGO_PKG_VERSION");
+            println!("cargo:rustc-env=CARGO_PKG_VERSION={}-{}", version, val);    
+        }
     }
     println!("cargo:rerun-if-env-changed=GSN2X_VERSION");
 }
