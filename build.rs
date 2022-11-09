55f88306e456f5b9937041afe0071e32c6856651
Jonas Wolf <jonas.wolf@jonaswolf.org>
2022-11-09 21:39:41 +0100

    Bump dependencies.

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     // If we set CARGO_PKG_VERSION this way, then it will override the default value, which is
     // taken from the `version` in Cargo.toml.
     if let Ok(val) = std::env::var("GSN2X_VERSION") {
-        if !val.is_empty() {
+        if !val.trim().is_empty() {
             let version = env!("CARGO_PKG_VERSION");
             println!("cargo:rustc-env=CARGO_PKG_VERSION={}-{}", version, val);    
         }
