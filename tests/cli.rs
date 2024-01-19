5978f4185b9c5e0b3959ecec5718ed8c1e8ef653
Jonas Wolf <jonas.wolf@jonaswolf.org>
2024-01-19 23:59:37 +0100

    Fix tests.

--- tests/cli.rs
+++ tests/cli.rs
@@ -501,7 +501,7 @@ mod integrations {
 
     #[test]
     fn issue339() -> Result<()> {
-        regression_renderings("tests/issue313.yaml", &[])?;
+        regression_renderings("tests/issue339.yaml", &[])?;
         Ok(())
     }
 
@@ -515,8 +515,8 @@ mod integrations {
     }
 
     #[test]
-    fn issue385() -> Result<()> {
-        regression_renderings("tests/issue385.yaml", &["-l", "layer1", "-l", "layer2"])?;
+    fn issue358() -> Result<()> {
+        regression_renderings("tests/issue358.yaml", &["-l", "layer1", "-l", "layer2"])?;
         Ok(())
     }
 
