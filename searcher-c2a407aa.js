7f1ebd9a885257c8d23ad763b06a7443dc930bef
jonasthewolf <jonas.wolf@jonaswolf.org>
2026-04-21 18:29:31 +0000

    Deploying to gh-pages from @ jonasthewolf/gsn2x@b71b0df7b245741dd19fb083a75e18ae50ca989c 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-fd2e927c.js',
+                path_to_root + 'searchindex-741849b0.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
